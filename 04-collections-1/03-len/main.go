b6f896baba0f5cd848997c4a87d4da8c983c9982
Ashutosh Bhardwaj <ashutoshbh@gmail.com>
2024-11-17 13:49:58 +1100

    Collections-03-solved

--- 04-collections-1/03-len/main.go
+++ 04-collections-1/03-len/main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 var colors = [5]string{"red", "green", "blue"}
 var systems = []string{"linux", "macos", "windows"}
+func NumberOfColors() int{
+	return len(colors)
+}
 
+func NumberOfSystems() int {
+	return len(systems)
+}
 func main() {
 	fmt.Println(NumberOfColors(), colors)
 	fmt.Println(NumberOfSystems(), systems)
