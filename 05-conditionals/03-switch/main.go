faa459287b17f29527fccad2d3395901e42a0536
Ashutosh Bhardwaj <ashutoshbh@gmail.com>
2024-11-17 21:44:32 +1100

    05-conditionls-03-switch-solved

--- 05-conditionals/03-switch/main.go
+++ 05-conditionals/03-switch/main.go
@@ -2,6 +2,11 @@ package main
 
 func Direction(d string) string {
 	switch d {
+	case "N": return "north"
+	case "S": return "south"
+	case "W": return "west"
+	case "E": return "east"
+	default: return "unknown"
 
 	}
 }
