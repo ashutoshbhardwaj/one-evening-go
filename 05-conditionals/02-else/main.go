8adea8ecc17a7aa3d6267863c721c19aa080dd8f
Ashutosh Bhardwaj <ashutoshbh@gmail.com>
2024-11-17 21:36:57 +1100

    05-Conditionls-02-else-solved

--- 05-conditionals/02-else/main.go
+++ 05-conditionals/02-else/main.go
@@ -2,4 +2,16 @@ package main
 
 func DescribeNumber(x int) string {
 
+	if x > 0 {
+		return "positive"
+	} else if x == 0 {
+		return "zero"
+	} else {
+		return "negative"
+	}
 }
+
+func main(){
+	DescribeNumber(5)
+}
+
