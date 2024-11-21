3052ef43bffe495959a6973de650a249583803ef
Ashutosh Bhardwaj <ashutoshbh@gmail.com>
2024-11-21 22:34:38 +1100

    06-errors-02-return-errors-solved

--- 06-errors/02-returning-errors/main.go
+++ 06-errors/02-returning-errors/main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
 func main() {
 	result, err := Divide(100, 50)
 	fmt.Println("Result:", result, "Error:", err)
 }
+
+// func Divide(x float64, y float64) (float64, error){
+// 	if y != 0 {
+// 		return x/y, nil 
+// 	} else {
+// 		return 0, errors.New("Divs")
+// 	}
+// }
+
+func Divide(x float64, y float64) (float64, error){
+		if y == 0 {
+			return 0, errors.New("Divs")
+		} 
+		return x / y, nil 
