05fbc1f06f98e8865629c15f9bd055e34a191aed
Ashutosh Bhardwaj <ashutoshbh@gmail.com>
2024-11-29 09:10:11 +1100

    07-collections-2-02-maps

---
+++ 07-collections-2/02-maps/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var (
+	Stats = map[string]int{}
+)
+
+func CreateUser(user string) {
+	fmt.Println(Stats)
+	fmt.Println("Creating user", user)
+	Stats["create"]++
+}
+
+func UpdateUser(user string) {
+	fmt.Println("Updating user", user)
+	Stats["update"]++
+}
+
+func PurgeStats() {
+	delete(Stats,"create")
+	delete(Stats,"update")
+}
