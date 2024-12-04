c4fc54851c2cb1f24985e2ad4dce0f80c9587efe
Ashutosh Bhardwaj <ashutoshbh@gmail.com>
2024-12-04 22:32:33 +1100

    07-collections-2-03-variadic-solved

--- 07-collections-2/03-variadic/main.go
+++ 07-collections-2/03-variadic/main.go
@@ -1,10 +1,17 @@
 package main
 
+
 func DebugLog(args ...string) []string {
+	Dump := []string{"[DEBUG]"}
+	return append(Dump, args...)
 }
 
 func InfoLog(args ...string) []string {
+	Dump := []string{"[INFO]"}
+	return append(Dump, args...) 
 }
 
 func ErrorLog(args ...string) []string {
+	Dump := []string{"[ERROR]"}
+	return append(Dump, args...) 
 }
